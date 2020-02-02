cee6954601ce82412b953b7c3b8a3bd2293b5e7c
Shubham Maurya <pnssoftwares7@gmail.com>
2020-02-02 19:08:23 +0530

    deleted useless files

--- app/src/main/java/com/pnstech/finalactivity/AboutApp.java
+++ app/src/main/java/com/pnstech/finalactivity/AboutApp.java
@@ -60,7 +60,7 @@ public class AboutApp extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/cyberatnit"));
-              startActivity(intent);
+                startActivity(intent);
             }
         });
     }
